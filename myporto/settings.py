0c7a98ad5666c6eefb24d73af73705920cf2ccbf
Taha Alsaity <121860432+Taha-Alsaity@users.noreply.github.com>
2024-10-22 02:50:51 +0200

    5th commit

--- myporto/settings.py
+++ myporto/settings.py
@@ -38,7 +38,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'your-email@gmail.com')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'tahadidanothe3@gmail.com')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'dfrr looz hwbe xgas')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
